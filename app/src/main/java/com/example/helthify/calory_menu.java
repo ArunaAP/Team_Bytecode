3821ab196c06c601e3409c36a61f3ee542a07f80
Aruna priyankara <90405801+ArunaAP@users.noreply.github.com>
2022-05-05 11:18:58 +0530

    create project folder

---
+++ app/src/main/java/com/example/helthify/calory_menu.java
@@ -0,0 +1,14 @@
+package com.example.helthify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class calory_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calory_menu);
+    }
+}
